5c084dbaf4eff1880bdf26371c2257516da2c5d7
swish-coder <asajja@asu.edu>
2020-12-06 18:17:09 -0700

    Moved gaussian sample generation to separate file.

---
+++ Experiments/sampling_from_gaussian.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+def is_symm(x):
+  return np.all(x==x.T)
+
+def is_pos_def(x):
+  return np.all(np.linalg.eigvals(x) > 0)
+
+def is_invertible(a):
+  return a.shape[0] == a.shape[1] and np.linalg.matrix_rank(a) == a.shape[0]
+
+def generate_psd_cov_mat(num_nodes,num_obs,using_R=True,force=True):
+  if using_R:
+    import subprocess
+    import pandas as pd
+    from os import path
+    if not path.exists('data/cov_mat.csv') or force:    
+      subprocess.check_call(['/usr/bin/Rscript', \
+          '--vanilla', \
+          'ggm.R', \
+          '--args', \
+          f'{num_nodes}', \
+          f'{num_obs}'], shell=False)
+    cov_mat = None
+    cov_mat_df = pd.read_csv('data/cov_mat.csv', header=None)
+    cov_mat = cov_mat_df.to_numpy()
+    adj_mat_df = pd.read_csv('data/adj_mat.csv', header=None)
+    adj_mat = adj_mat_df.to_numpy()
+
+    assert is_invertible(cov_mat)==True
+    assert is_pos_def(cov_mat)==True
+    assert is_symm(cov_mat)==True
+    
+    return adj_mat, cov_mat
+  else:
+    # Can try to write a Python script to mimic the R script.
+    pass
+
--- Experiments/scarlet.py
+++ Experiments/scarlet.py
@@ -9,45 +9,9 @@ Original file is located at
 import copy
 import numpy as np
 from numpy.linalg import norm
+from Experiments.sampling_from_gaussian import generate_psd_cov_mat
 from utils import plot_error_vs_samples
 np.random.seed((3,14159))
-
-
-def generate_psd_cov_mat(num_nodes,num_obs,using_R=True,force=False):
-  if using_R:
-    import subprocess
-    import pandas as pd
-    from os import path
-    if not path.exists('data/cov_mat.csv') or force:    
-      subprocess.check_call(['/usr/bin/Rscript', \
-          '--vanilla', \
-          'ggm.R', \
-          '--args', \
-          f'{num_nodes}', \
-          f'{num_obs}'], shell=False)
-    cov_mat = None
-    cov_mat_df = pd.read_csv('data/cov_mat.csv', header=None)
-    cov_mat = cov_mat_df.to_numpy()
-    adj_mat_df = pd.read_csv('data/adj_mat.csv', header=None)
-    adj_mat = adj_mat_df.to_numpy()
-
-    assert is_invertible(cov_mat)==True
-    assert is_pos_def(cov_mat)==True
-    assert is_symm(cov_mat)==True
-    
-    return adj_mat, cov_mat
-  else:
-    # Can try to write a Python script to mimic the R script.
-    pass
-
-def is_symm(x):
-  return np.all(x==x.T)
-
-def is_pos_def(x):
-  return np.all(np.linalg.eigvals(x) > 0)
-
-def is_invertible(a):
-  return a.shape[0] == a.shape[1] and np.linalg.matrix_rank(a) == a.shape[0]
 
 def normalize_data(B,v_max,lamda,X_samples,Y_samples):
   norm_const = 1/((B)*np.sqrt(v_max*(lamda + 1)))
